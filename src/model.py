96139e6e97e9f365171a110264b8c1ef47f19595
Srija8606 <srija8606@gmail.com>
2026-02-14 16:27:37 +0530

    Add model building function using XGBClassifier

---
+++ src/model.py
@@ -0,0 +1,14 @@
+from xgboost import XGBClassifier
+
+def build_model():
+    model = XGBClassifier(
+        n_estimators=100,
+        max_depth=3,
+        learning_rate=0.1,
+        subsample=0.8,
+        colsample_bytree=0.8,
+        random_state=42,
+        eval_metric="logloss"
+    )
+
+    return model
